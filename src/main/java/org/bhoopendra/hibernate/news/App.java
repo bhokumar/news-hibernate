12237f36eb43a59934007bd8f9fb78a04424d51b
bhoopendra <bhoopendra.akgec@gmail.com>
2017-07-07 22:56:21 +0530

    creating initial for hibernate based project

---
+++ src/main/java/org/bhoopendra/hibernate/news/App.java
@@ -0,0 +1,13 @@
+package org.bhoopendra.hibernate.news;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
