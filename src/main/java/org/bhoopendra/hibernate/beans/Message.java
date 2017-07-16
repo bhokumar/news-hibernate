85196e018c219f3560fdef794741a6cd3bf62c4a
bhoopendra <bhoopendra.akgec@gmail.com>
2017-07-16 23:04:54 +0530

    Adding bean class

---
+++ src/main/java/org/bhoopendra/hibernate/beans/Message.java
@@ -0,0 +1,29 @@
+package org.bhoopendra.hibernate.beans;
+
+public class Message {
+	private Long Id;
+	private String text;
+	private Message nextMessage;
+	public Message() {
+	}
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Message getNextMessage() {
+		return nextMessage;
+	}
+	public void setNextMessage(Message nextMessage) {
+		this.nextMessage = nextMessage;
+	}
+	
+	
+}
