f52feac124b60b69ee0ad327a4f55193e03157ea
bhoopendra <bhoopendra.akgec@gmail.com>
2017-07-18 23:47:07 +0530

    Applying logging mechanis in hibernate project

--- src/main/java/org/bhoopendra/hibernate/news/App.java
+++ src/main/java/org/bhoopendra/hibernate/news/App.java
@@ -2,6 +2,8 @@ package org.bhoopendra.hibernate.news;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.bhoopendra.hibernate.beans.Address;
 import org.bhoopendra.hibernate.beans.Employee;
@@ -19,13 +21,23 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Logger logger = Logger.getLogger(App.class.getName());
+    	logger.log(Level.INFO, "Application getting started!");
     	List<Laptop> laptops = new ArrayList<Laptop>();
+    	logger.log(Level.INFO, "List of laptops is created");
     	laptops.add(new Laptop(101, "HP", "Windows", new Employee("bhoopendra", 5001, new Address("street", "Budaun", "UP", 243601), null)));
+    	logger.log(Level.INFO, "Creating laptop object");
     	Employee employee = new Employee("mohan", 12457, new Address("street number -5", "delhi", "Uttar Pradesh", 201301),laptops);
+    	logger.log(Level.INFO, "Getting Session Factory object");
     	SessionFactory sessionFactory = SessionFactoryCreator.getSessionFactoryInstance();
+    	logger.log(Level.INFO, "Got session factory object !");
+    	logger.log(Level.INFO, "Creating Session object!");
     	Session session = sessionFactory.openSession();
+    	logger.log(Level.INFO, "Session object created!");
     	Transaction transaction = session.beginTransaction();
+    	logger.log(Level.INFO, "Transaction created");
     	session.save(employee);
+    	logger.log(Level.INFO, "Object saved");
     	transaction.commit();
     	Employee employee2 =session.get(Employee.class, 12457);
     	System.out.println(employee2);
