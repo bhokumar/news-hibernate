1a2fd8877a3be1876e8dfca70bd77695ae3b9536
bhokumar <bhoopendra.akgec@gmail.com>
2017-07-20 19:27:24 +0530

    Adding files

--- src/main/java/org/bhoopendra/hibernate/beans/UserDetails.java
+++ src/main/java/org/bhoopendra/hibernate/beans/UserDetails.java
@@ -1,45 +1,66 @@
 package org.bhoopendra.hibernate.beans;
 
-
 import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 @Entity
-@Table(name="USER_DETAILS")
+@Table(name = "USER_DETAILS")
 public class UserDetails {
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private int userId;
-	private String userName;
-	private Date joinedDate;
 	
+	@Lob
+	private String description;
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	private String userName;
+
+	@Temporal(TemporalType.DATE)
+	private Date joinedDate;
+
 	public UserDetails(String userName, Date joinedDate) {
 		super();
 		this.userName = userName;
 		this.joinedDate = joinedDate;
 	}
-	
+
 	public int getUserId() {
 		return userId;
 	}
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public Date getJoinedDate() {
 		return joinedDate;
 	}
+
 	public void setJoinedDate(Date joinedDate) {
 		this.joinedDate = joinedDate;
 	}
-	
+
 }
