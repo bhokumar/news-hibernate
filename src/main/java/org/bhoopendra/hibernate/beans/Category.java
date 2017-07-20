da17d5f51e0616dba2cfe8ca2c832ae7daa69cce
bhokumar <bhoopendra.akgec@gmail.com>
2017-07-20 11:50:50 +0530

    Changing files

--- src/main/java/org/bhoopendra/hibernate/beans/Category.java
+++ src/main/java/org/bhoopendra/hibernate/beans/Category.java
@@ -5,6 +5,13 @@ import java.util.Set;
 
 public class Category implements Serializable {
 	private static final long serialVersionUID = -764864347811810103L;
+	private long id;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	private String name;
 	private Category parentCategory;
 	private Set<Category> childCategories;
@@ -39,42 +46,5 @@ public class Category implements Serializable {
 
 	public void setParentCategory(Category parentCategory) {
 		this.parentCategory = parentCategory;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((childCategories == null) ? 0 : childCategories.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((parentCategory == null) ? 0 : parentCategory.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Category other = (Category) obj;
-		if (childCategories == null) {
-			if (other.childCategories != null)
-				return false;
-		} else if (!childCategories.equals(other.childCategories))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (parentCategory == null) {
-			if (other.parentCategory != null)
-				return false;
-		} else if (!parentCategory.equals(other.parentCategory))
-			return false;
-		return true;
 	}
 }
